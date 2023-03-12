59a1e360bae0d32991dfe6444dab1be8584bc392
Desoroxxx <desoroxxx@gmail.com>
2023-03-12 13:07:05 +0100

    Initial Commit

---
+++ src/main/java/io/redstudioragnarok/rcw/RCW.java
@@ -0,0 +1,8 @@
+package io.redstudioragnarok.rcw;
+
+import io.redstudioragnarok.rcw.utils.ModReference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ModReference.id, name = ModReference.name, version = ModReference.version)
+public class RCW {
+}
---
+++ src/main/java/io/redstudioragnarok/rcw/utils/ModReference.java
@@ -0,0 +1,15 @@
+package io.redstudioragnarok.rcw.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * This class defines constants for RCW.
+ */
+public class ModReference {
+
+    public static final String id = "@ID@";
+    public static final String name = "@NAME@";
+    public static final String version = "@VERSION@";
+    public static final Logger log = LogManager.getLogger(id);
+}
