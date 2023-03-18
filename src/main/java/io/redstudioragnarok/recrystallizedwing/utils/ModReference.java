074989c3a5df8e14a08489d4c6621547a1445f84
Desoroxxx <desoroxxx@gmail.com>
2023-03-18 18:45:20 +0100

    Fix Name

--- src/main/java/io/redstudioragnarok/recrystallizedwing/utils/ModReference.java
+++ src/main/java/io/redstudioragnarok/recrystallizedwing/utils/ModReference.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class ModReference {
 
     public static final String id = "@ID@";
-    public static final String name = "@NAME@";
+    public static final String name = "Re-Crystallized Wing";
     public static final String version = "@VERSION@";
     public static final Logger log = LogManager.getLogger(id);
 }
